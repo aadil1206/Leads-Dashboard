6c3ba7e64e43ff4fd7df82de8edc60923f3cd114
Mohammad Adil <mohammadadil@globussoft.in>
2025-08-20 17:13:08 +0530

    fixed ui part

--- Frontend/vite-project/src/components/FormActions.jsx
+++ Frontend/vite-project/src/components/FormActions.jsx
@@ -2,7 +2,7 @@ export const FormActions = ({ onCancel, isSubmitting = false }) => (
   <div className="col-span-2 flex justify-end gap-3 mt-6">
     <button
       type="button"
-      className="px-4 py-2 rounded-md border text-[#020817] hover:bg-gray-100 transition-colors"
+      className="px-4 h-[40px] flex items-center  rounded-[6px] border text-[#020817] hover:bg-gray-100 transition-colors"
       onClick={onCancel}
     >
       Cancel
@@ -10,7 +10,7 @@ export const FormActions = ({ onCancel, isSubmitting = false }) => (
     <button
       type="submit"
       disabled={isSubmitting}
-      className={`px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 transition-colors ${
+      className={`px-4 h-[40px] flex items-center rounded-[6px] bg-blue-600 text-white hover:bg-blue-700 transition-colors ${
         isSubmitting ? "opacity-70 cursor-not-allowed" : ""
       }`}
     >
